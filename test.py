d58669a2dfe148cf91120f8ef4e77a117f904a57
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-02-24 00:43:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import color_codes as g
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
-  major_color, minor_color = pair_def.get_color_from_pair_number(pair_number)
+  major_color, minor_color = color_pair.get_color_from_pair_number(pair_number)
   assert(major_color == expected_major_color)
   assert(minor_color == expected_minor_color)
 
 
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = pair_def.get_pair_number_from_color(major_color, minor_color)
+  pair_number = color_pair.get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
 
 def test_user_manual_length(expected_pair_count):
