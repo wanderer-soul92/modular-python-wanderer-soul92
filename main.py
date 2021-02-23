a2bf61f54fbcec16befafe18692e39b99636de7c
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-02-24 00:42:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import test
+import color_codes as g
 
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
+
+x= g.get_user_manual()
 
 if __name__ == '__main__':
   test.test_number_to_pair(4, 'White', 'Brown')
@@ -9,4 +12,6 @@ if __name__ == '__main__':
   test.test_pair_to_number('Black', 'Orange', 12)
   test.test_pair_to_number('Violet', 'Slate', 25)
   test.test_pair_to_number('Red', 'Orange', 7)
+  test.get_user_manual(25)
+  print(x)
   print('Done :)')
